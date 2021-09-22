c595c22b735d463b121291d9af5dd00fb04ad63e
Khotso Mokhethi <Mokhetkc@hotmail.com>
2021-09-22 11:01:28 +0200

    Refactoring
    Refactoring

--- KhotsoCBookStore.API/Controllers/HomeController.cs
+++ KhotsoCBookStore.API/Controllers/HomeController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using WebMvc.Models;
+
 using MassTransit;
 using System.IO;
 
@@ -73,10 +71,10 @@ namespace KhotsoCBookStore.API.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
