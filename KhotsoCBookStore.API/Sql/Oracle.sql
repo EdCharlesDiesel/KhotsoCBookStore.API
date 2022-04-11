3ef8d1af96a6f47aaa673fc45be2ec680cdab10f
Khotso Mokhethi <Mokhetkc@hotmail.com>
2022-04-11 18:27:29 +0200

    SQL
    Working with SQL, TSQL, PL/SQL and progres

---
+++ KhotsoCBookStore.API/Sql/Oracle.sql
@@ -0,0 +1,121 @@
+SELECT* FROM HumanResources.Department WHERE
+(DepartmentID = 11 OR GroupName IS NOT NULL OR ModifiedDate <= '2008-04-30')
+AND DepartmentID= 10;
+
+--PROMBLEM 1.7
+SELECT Name  || '-' ||GroupName as [Id and Name] FROM HumanResources.Department WHERE
+modifiedDate <= '2008-04-30';
+
+--PROMBLEM 1.8
+SELECT BusinessEntityID,Rate,
+	CASE WHEN Rate >=100 THEN 'OVERPAID'
+		WHEN Rate >=50 THEN 'OK'
+		ELSE 'UNDERPAID' 
+		END AS [SALARY RATE]
+FROM  [HumanResources].[EmployeePayHistory];
+
+--PROMBLEM 1.9
+SELECT * FROM  [HumanResources].[EmployeePayHistory]
+WHERE ROWNUM <= 5;
+
+--PROMBLEM 1.10
+SELECT * FROM
+(SELECT LoginID,JobTitle 
+FROM  [HumanResources].Employee
+ORDER BY DBMS_RANDOM.value()
+)
+WHERE ROWNUM <= 5;
+
+--PROMBLEM 1.12
+SELECT 
+	CASE WHEN [OrganizationLevel] IS NOT NULL THEN [OrganizationLevel]
+	ELSE 0
+	END
+FROM  [HumanResources].[Employee];
+
+SELECT COALESCE([OrganizationLevel],0) AS [OrganizationLevel]
+ from [HumanResources].[Employee];
+
+ --PROMBLEM 1.13
+SELECT LoginID,JobTitle 
+FROM  [HumanResources].Employee WHERE [OrganizationLevel] =3
+AND ([JobTitle] LIKE '%WC60')
+
+--PROMBLEM 2.3
+SELECT LoginID,JobTitle 
+FROM  [HumanResources].Employee ORDER BY SUBSTR(LoginID,LEN(LoginID)-1);
+
+--PROMBLEM 2.4
+CREATE VIEW [Login_ID]
+AS
+SELECT LoginID + ' - ' +  NationalIDNumber AS [Login and ID]
+FROM  [HumanResources].[Employee];
+
+SELECT [Login and ID]
+FROM [Login_ID]
+ ORDER BY REPLACE([Login and ID],
+		  REPLACE(
+			TRANSLATE([Login and ID],'48#####','##########'),'#',''),'');
+
+--PROMBLEM 2.5
+SELECT [DepartmentID],[StartDate],[EndDate]
+ FROM [HumanResources].EmployeeDepartmentHistory
+ ORDER BY [EndDate] NULLS FIRST;
+
+SELECT [DepartmentID],[StartDate],[EndDate]
+ FROM [HumanResources].EmployeeDepartmentHistory
+ ORDER BY [EndDate] NULLS LAST;
+
+--PROMBLEM 2.6
+SELECT [BusinessEntityID], [NationalIDNumber], [LoginID],[OrganizationLevel],[JobTitle]
+FROM   [HumanResources].Employee
+	ORDER BY CASE WHEN [JobTitle] LIKE 'Senior%' 
+	THEN [OrganizationLevel]
+	ELSE [BusinessEntityID]
+	END;
+
+--PROMBLEM 3.1
+SELECT [ModifiedDate],[BusinessEntityID] FROM [HumanResources].[Employee]
+WHERE [ModifiedDate] >='2009-01-13 00:00:00.000'
+UNION ALL 
+SELECT [ModifiedDate],[BusinessEntityID] FROM [HumanResources].[EmployeeDepartmentHistory]
+
+--PROMBLEM 3.2
+SELECT e.[LoginID], e.[JobTitle],j.[Resume]
+FROM [HumanResources].[Employee] e, [HumanResources].[JobCandidate] j
+WHERE e.OrganizationLevel = 3
+
+SELECT e.[LoginID], e.[JobTitle],j.[Resume]
+FROM [HumanResources].[Employee] e INNER JOIN [HumanResources].[JobCandidate] j
+ON e.BusinessEntityID = j.BusinessEntityID
+WHERE e.OrganizationLevel = 3
+
+--PROMBLEM 3.3
+CREATE VIEW EmployeeMinView
+AS
+SELECT [BusinessEntityID],
+[NationalIDNumber],
+[OrganizationLevel],
+[JobTitle],
+[BirthDate],
+[HireDate]
+FROM [HumanResources].Employee
+
+
+SELECT [NationalIDNumber],[OrganizationLevel],
+FROM [HumanResources].Employee
+WHERE (ename,job,sal) IN (
+SELECT ename,job,sal FROM [HumanResources].[JobCandidate]
+INTERSECT
+SELECT ename,job,sal from EmployeeMinView
+
+--PROMBLEM 3.4
+SELECT [BusinessEntityID] FROM [HumanResources].JobCandidate
+MINUS
+SELECT [BusinessEntityID] FROM [HumanResources].Employee
+
+--PROMBLEM 3.5	 
+SELECT J.*
+FROM [HumanResources].JobCandidate J left outer join [HumanResources].Employee e
+ON (J.BusinessEntityID = e.BusinessEntityID)
+WHERE J.BusinessEntityID IS NULL
