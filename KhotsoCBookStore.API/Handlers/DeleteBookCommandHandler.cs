5869fc8103e3898a16d94af40951d90d2685c343
Khotso Mokhethi <Mokhetkc@hotmail.com>
2022-06-01 19:41:42 +0200

    .

--- KhotsoCBookStore.API/Handlers/DeleteBookCommandHandler.cs
+++ KhotsoCBookStore.API/Handlers/DeleteBookCommandHandler.cs
@@ -1,26 +1,26 @@
-﻿using DDD.ApplicationLayer;
-using DDD.DomainLayer;
-using KhotsoCBookStore.API.Commands;
-using StarPeaceAdminHubDomain.IRepositories;
-using System.Threading.Tasks;
+﻿// using DDD.ApplicationLayer;
+// using DDD.DomainLayer;
+// using KhotsoCBookStore.API.Commands;
+// using StarPeaceAdminHubDomain.IRepositories;
+// using System.Threading.Tasks;
 
-namespace KhotsoCBookStore.API.Handlers
-{
-    public class DeleteBookCommandHandler : ICommandHandler<DeleteBookCommand>
-    {
-        IBookRepository repo;
-        IEventMediator mediator;
-        public DeleteBookCommandHandler(IBookRepository repo, IEventMediator mediator)
-        {
-            this.repo = repo;
-            this.mediator = mediator;
-        }
-        public async Task HandleAsync(DeleteBookCommand command)
-        {
-            // var deleted = await repo.Delete(command.BookId);
-            // if (deleted != null)
-            //     await mediator.TriggerEvents(deleted.DomainEvents);
-            // await repo.UnitOfWork.SaveEntitiesAsync();
-        }
-    }
-}
+// namespace KhotsoCBookStore.API.Handlers
+// {
+//     public class DeleteBookCommandHandler : ICommandHandler<DeleteBookCommand>
+//     {
+//         IBookRepository repo;
+//         IEventMediator mediator;
+//         public DeleteBookCommandHandler(IBookRepository repo, IEventMediator mediator)
+//         {
+//             this.repo = repo;
+//             this.mediator = mediator;
+//         }
+//         public async Task HandleAsync(DeleteBookCommand command)
+//         {
+//             var deleted = await repo.Delete(command.BookId);
+//             if (deleted != null)
+//                 await mediator.TriggerEvents(deleted.DomainEvents);
+//             await repo.UnitOfWork.SaveEntitiesAsync();
+//         }
+//     }
+// }
